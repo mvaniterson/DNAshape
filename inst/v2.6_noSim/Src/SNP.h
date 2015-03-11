a56a31482487076ea86d63b9c60d6840d93156fa
mvaniterson <mviterson@gmail.com>
2015-03-11 13:10:54 +0100

    cleanup inst

--- inst/v2.6_noSim/Src/SNP.h
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-#include "utilities.h"
-#include "map.h"
-
-void SNP_distribution(DNA_to_properties&);
-
-void predict_mgw_to_vector(DNA_to_properties&,std::string,double_vector&);
-
-double pairwise_Euclidean(double_vector&,double_vector&);
