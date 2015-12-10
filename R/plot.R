0586a26810081ad82fd64a3e7c3e77a7f9567071
mvaniterson <mviterson@gmail.com>
2015-12-10 14:27:31 +0100

    extend plotting capabilities

--- R/plot.R
+++ R/plot.R
@@ -1,46 +1,44 @@
-##' plot minor groove width as function of the sequence
+.plotShape <- function(x, y, ylab, seq, add=FALSE, ...) {
+    args <- list(...)
+    col <- ifelse(any(names(args) == "col"), args[["col"]], "black")
+    if(!add) {
+        op <- par(mar=c(4,5,4,2))
+        plot(x, y, type="b", ylab=ylab, xaxt="n", bty="n", xlab="", ...)
+        mtext(unlist(strsplit(as.character(seq), "")), side=1, at=1:length(x), line=0, col=col)
+        abline(h=axTicks(2), col="grey", lty=3)
+        par(op)
+    }
+    else {       
+        points(x, y, type="b", ...)
+        mtext(unlist(strsplit(as.character(seq), "")), side=1, at=1:length(x), line=2,  col=col)
+    }
+}
+
+##' plot shape as function of function
 ##'
 ##' Optionally one other sequence can be added
-##' @title plot Minor groove width
+##' @title plot shape as function of sequences
 ##' @param shape predicted shapes
 ##' @param seq DNAstring containing sequence
+##' @param type "mgw", "prot", "roll" or "helt"
 ##' @param add TRUE/FALSE
 ##' @param ... optional plotting parameters
 ##' @return called for its side-effect
 ##' @author mvaniterson
 ##' @export
-plotMGW <- function(shape, seq, add=FALSE, ...) {    
-    y <- shape$minor_ave
-    y <- c(NA, NA, y, NA)
-    x <- 1:length(y)
-    args <- list(...)
-    col <- ifelse(any(names(args) == "col"), args[["col"]], 1)    
-    if(!add) {
-        op <- par(mar=c(4,5,4,2))
-        plot(x, y, type="b", ylab=expression("Minor groove width["*ring(A)*"]"), xaxt="n", bty="n", xlab="", ...)
-        mtext(unlist(strsplit(as.character(seq), "")), side=1, at=1:length(x), line=0, col=col)
-        abline(h=axTicks(2), col="grey", lty=3)
-        par(op)
-    }
-    else {
-        args <- list(...)
-        col <- ifelse(any(names(args) == "col"), args[["col"]], 1)
-        points(x, y, type="b", ...)
-        mtext(unlist(strsplit(as.character(seq), "")), side=1, at=1:length(x), line=2,  col=col)
-    }    
+plotShape <- function(shape, seq, type=c("mgw", "prot", "roll", "helt"), add=FALSE, ...){
+    type <- match.arg(type)
+    switch(type,
+           mgw = .plotShape(x = seq(length=nrow(shape)+3),
+               y = c(NA, NA, shape$minor_ave, NA),
+               ylab = expression("Minor groove width["*ring(A)*"]"), seq=seq, add=add, ...),
+           prot = .plotShape(x = seq(length=nrow(shape)+3),
+               y = c(NA, NA, shape$propel_ave, NA),
+               ylab = expression("Propeller Twist["*degree*"]"), seq=seq, add=add, ...),
+           roll = .plotShape(x = seq(from=0.5, length=nrow(shape)+2),
+               y = c(NA, NA, shape$roll_ave),
+               ylab = expression("Roll["*degree*"]"), seq=seq, add=add, ...),
+           helt = .plotShape(x = seq(from=0.5, length=nrow(shape)+2),
+               y = c(NA, NA, shape$twist_ave),
+               ylab = expression("Helix Twist["*degree*"]"), seq=seq, add=add, ...))
 }
-
-
-plotROLL <- function(){
-}
-
-plotPROT <- function(){
-    
-}
-
-plotHELT <- function(){
-    
-}
-
-
-    
