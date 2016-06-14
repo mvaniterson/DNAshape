a8a8cafdfa676dc782633d57cf287eb174aef722
mvaniterson <mviterson@gmail.com>
2016-06-14 14:22:48 +0100

    fix bug shapediff

--- R/seq2shape.R
+++ R/seq2shape.R
@@ -20,15 +20,15 @@
     ##average pair properties
     z <- c(NA, NA)
     for(i in 3:4) {
-            x <- as.numeric(shape[,i])
-            y <- as.numeric(shape[,i+1])
-            shape[1, i] <- x[1]
-            if(length(x) > 1)
-                shape[-1,i] <- 0.5*(x[-1] + y[-length(y)])
-            z <- c(z, y[length(y)])
-            shape <- shape[,-i-1,drop=FALSE]
-        }
-    
+        x <- as.numeric(shape[,i])
+        y <- as.numeric(shape[,i+1])
+        shape[1, i] <- x[1]
+        if(length(x) > 1)
+            shape[-1,i] <- 0.5*(x[-1] + y[-length(y)])
+        z <- c(z, y[length(y)])
+        shape <- shape[,-i-1,drop=FALSE]
+    }
+
     ##reshape output adding step bases and pairs
     shape <- rbind(shape, N=z)
     colnames(shape) <- gsub("1", "", colnames(shape))
@@ -61,6 +61,29 @@ seq2shape  <- function(sequence, kmerLength=5) {
         return(.seq2shape(as.character(sequence)))
     else if(class(sequence) == "DNAStringSet")
         return(lapply(as.character(sequence), .seq2shape))
-    else 
+    else
         stop(paste("Unknown sequence input:", class(sequence)))
+}
+
+##' calculate shape difference
+##' 
+##' Calculate shape difference between two sequences
+##' @title shapediff
+##' @param seq1 DNA sequence 1
+##' @param seq2 DNA sequence 2
+##' @param type shape difference measure rmsd or eucl.
+##' @return shape difference
+##' @author mvaniterson
+##' @export
+shapediff <- function(seq1, seq2, type=c("rmsd", "eucl")) {
+    type <- match.arg(type)    
+    if(nchar(as.character(seq1)) != nchar(as.character(seq2)))
+        stop("Sequence lengths do not match!")
+
+    shape1 <- seq2shape(seq1)
+    shape2 <- seq2shape(seq2)
+
+    switch(type,
+           eucl = colSums((shape1 - shape2)^2, na.rm=TRUE),
+           rmsd = sqrt(colSums((shape1 - shape2)^2, na.rm=TRUE)/nrow(shape1)))
 }
